51cd930af776b77ce48d4b4ea65a322bf05be3e9
Nic Orchard <cash@GG-McAir.local>
2022-05-11 12:48:36 +0200

    functions

---
+++ funcs.py
@@ -0,0 +1,29 @@
+import pickle
+import pandas as pd
+import numpy as np
+from sktime.forecasting.arima import AutoARIMA
+import pymysql
+
+def do_train(data):
+    """Comprobar el MAPE y hacer retrain con autoarima si el mayor de 0.2"""
+    data.Date = pd.to_datetime(data.Date, dayfirst=True)
+    data = data.set_index('Date')
+    weekly = data.groupby(pd.Grouper(freq='W')).sum()
+    arima = AutoARIMA(start_p = 1, start_q = 1)
+    arima.fit(weekly)
+    with open('arima.pickle', 'wb') as f:
+        pickle.dump(arima, f)
+    return "refit done"
+    
+def do_predict(n_weeks=1):
+    with open('arima.pickle', 'rb') as f:
+        model = pickle.load(f)
+    predictions = model.predict(fh=list(range(1,n_weeks+1)))
+    return predictions
+
+def get_data(endpoint, port, password, user):
+    """Conectarse a AWS y leer los datos"""
+    db = pymysql.connect(host = endpoint,
+                        user = user,
+                        password = password,
+                        cursorclass = pymysql.cursors.DictCursor)
