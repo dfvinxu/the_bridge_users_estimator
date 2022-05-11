970100274fdfb86f78c6639e8a0b207e9e985342
Nic Orchard <cash@GG-McAir.local>
2022-05-11 12:40:21 +0200

    Nic flask_try

--- flask_try.py
+++ flask_try.py
@@ -1,29 +1,29 @@
 from pkgutil import get_data
+import os
+from sqlalchemy import create_engine
 from flask import Flask, jsonify, request
 from flask_restful import Api, Resource
 from sklearn.model_selection import train_test_split
 from sklearn.metrics import mean_squared_error
-import pandas as pd
-import os
-import markdown
 from funcs import do_train, do_predict, get_data
 from credentials import endpoint, port, password, user
 
 ########################################################################################
 
+os.chdir(os.path.dirname(__file__))
+
 app = Flask(__name__)
 app.config["DEBUG"] = True
 
-#os.chdir(os.path.dirname(__file__))
-
 @app.route('/', methods=['GET'])
 def home():
+    return 'Hello'
     # Open the home file
-    with open(os.path.dirname(app.root_path)+'home.md','r') as markdown_file:
-        # read content 
-        content = markdown_file.read()
-        #convert to HTML
-        return markdown.markdown(content)
+    # with open(os.path.dirname(app.root_path)+'home.md','r') as markdown_file:
+    #     # read content 
+    #     content = markdown_file.read()
+    #     #convert to HTML
+    #     return markdown.markdown(content)
 
 @app.route('/api/predict', methods=['GET'])
 def predict():
@@ -43,16 +43,11 @@ def update_data():
     cursor = db.cursor()
     # necesita que el usuario le de los datos nuevos (fecha y num. usuarios)
     df = request.args['df']
+    # create sqlalchemy engine
+    engine = create_engine("mysql+pymysql://{user}:{pw}@{host}/{db}".format(user = user, pw = password, host = endpoint, db = 'country_database'))
     # insertamos todo el dataframe
     df.to_sql(name='new_users', con=engine, if_exists= 'append', index=False)
     # commit los cambios 
     db.commit()
-
-@app.route('/api/updatemodel', methods=['GET'])
-def new_model():
-    # necesita leer los datos para crear un modelo nuevo
-    data = pd.read_csv('data/Advertising.csv', index_col=0)
-    # train new model and save it into new pickle
-
 
 app.run()
